b9954f89ebaa799311f23e63f5bf41edb7d5d2ca
Dwan Veloza <dwanpaez@gmail.com>
2024-08-28 21:37:01 -0500

    allow camel case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
 import RootLayout from "./layout";
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export default function Home(): JSX.Element {
   return (
     <RootLayout>
